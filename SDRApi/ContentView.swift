d0c9633b4188595794e6b9089f229c039415fe27
Douglas Adams <douglas.adams@me.com>
2023-12-20 16:56:55 -0500

    Initial Commit

---
+++ SDRApi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SDRApi
+//
+//  Created by Douglas Adams on 12/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SDRApi/SDRApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDRApiApp.swift
+//  SDRApi
+//
+//  Created by Douglas Adams on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SDRApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
