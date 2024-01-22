a9fa4b214971f6ed1fe769a6650f4d860cad5f0c
Douglas Adams <douglas.adams@me.com>
2024-01-22 11:37:06 -0500

    Initial Commit

---
+++ SDRTestViewer/SDRTestViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDRTestViewerApp.swift
+//  SDRTestViewer
+//
+//  Created by Douglas Adams on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SDRTestViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
